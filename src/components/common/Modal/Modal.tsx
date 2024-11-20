f8299c6d0023d24bf388214954be56f87f8832a4
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-20 17:28:49 +0900

    chore : modal click event 수정

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -6,10 +6,13 @@ interface ModalProps {
     clickEvent: () => void;
 }
 
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children, clickEvent }: ModalProps) => {
     return (
         <Overlay>
-            <div className="bg-white w-[370px] h-[200px] rounded-2xl p-2 relative shadow-lg animate-fadeIn">
+            <div
+                className="bg-white w-[370px] h-[200px] rounded-2xl p-2 relative shadow-lg animate-fadeIn"
+                onClick={clickEvent}
+            >
                 <div className="relative h-full">{children}</div>
             </div>
         </Overlay>
