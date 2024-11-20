09106d764ff8d1ca2b1f6153a47dad7fe9df3478
jaja <dkwjdals35@gmail.com>
2024-11-20 15:48:01 +0900

    style : Tailwind  버튼 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: '#D1D5DB'
+            }
+        }
     },
-    plugins: []
+    plugins: [
+        ({ addComponents }) => {
+            addComponents({
+                '.btn-base': {
+                    '@apply bg-primary w-full rounded p-6 text-black hover:opacity-80':
+                        ''
+                },
+
+                '.text-btn': {
+                    '@apply text-black hover:underline': ''
+                }
+            });
+        }
+    ]
 };
