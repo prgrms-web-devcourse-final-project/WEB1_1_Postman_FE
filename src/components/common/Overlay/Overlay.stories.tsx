cbf22d3a0cd99b44247a8b80f8918f70a113ce2c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-20 16:35:37 +0900

    feat : overlay 스토리 작성

---
+++ src/components/common/Overlay/Overlay.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Overlay } from './Overlay';
+
+const meta: Meta<typeof Overlay> = {
+    component: Overlay,
+    title: 'Overlay',
+    tags: ['autodocs'],
+    argTypes: {}
+};
+export default meta;
+
+type Story = StoryObj<typeof Overlay>;
+
+export const Default: Story = {
+    args: {
+        children: <>즐겁단</>
+    }
+};
