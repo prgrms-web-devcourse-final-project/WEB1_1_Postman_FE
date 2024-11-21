ece12743a965bee67ae2fc4d93b62b240b34f285
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-21 16:42:51 +0900

    chore : 경로 수정

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal } from '@/components/common/Modal/Modal';
+import { Modal } from '@/components/Common/Modal/Modal';
 import { useState } from 'react';
 
 /**
