3e333d3f841f7ef788c4eb5c3cebe2e4f4e0e401
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-21 16:52:33 +0900

    feat : navigateContainer 개발

---
+++ src/components/MapPage/NavigateContainer/NavigateContainer.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { NavigateContainer } from './NavigateContainer';
+
+const meta: Meta<typeof NavigateContainer> = {
+    component: NavigateContainer,
+    title: 'NavigateContainer',
+    tags: ['autodocs'],
+    argTypes: {}
+};
+export default meta;
+
+type Story = StoryObj<typeof NavigateContainer>;
+
+export const Default: Story = {
+    args: {
+        title: '가을 바람',
+        distance: '15km',
+        count: 4,
+        clickEvent: () => {
+            alert('헬로');
+        }
+    }
+};
