114adc1c8d3bdc06909bf8ffd35cc8181ba21703
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-25 11:04:09 +0900

    chore : 모든 파일 삭제

--- src/components/Common/BottleLetter/Bottle/Bottle.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Bottle = () => {
-    return (
-        <div className="">
-            <img src="/유리병_샘플.png" className="h-[100px]" />
-        </div>
-    );
-};
