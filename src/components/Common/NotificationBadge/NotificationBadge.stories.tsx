a883e7e0635873307ce1c43c4224c91efed88be4
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-25 11:32:00 +0900

    fix : devlope fix 수정 (#69)

--- src/components/Common/NotificationBadge/NotificationBadge.stories.tsx
+++ src/components/Common/NotificationBadge/NotificationBadge.stories.tsx
@@ -13,11 +13,19 @@ export default meta;
 type Story = StoryObj<typeof NotificationBadge>;
 
 export const notificationMin: Story = {
+<<<<<<< HEAD
     args: { badgeType: 'basic', count: 1 }
 };
 
 export const notificationMax: Story = {
     args: { badgeType: 'basic', count: 100 }
+=======
+    args: { badgeType: 'notification', count: 1 }
+};
+
+export const notificationMax: Story = {
+    args: { badgeType: 'notification', count: 100 }
+>>>>>>> main
 };
 
 export const dday: Story = {
--- src/components/Common/NotificationBadge/NotificationBadge.tsx
+++ src/components/Common/NotificationBadge/NotificationBadge.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 interface NotificationBadgeProps {
+<<<<<<< HEAD
     badgeType: 'basic' | 'dday';
+=======
+    badgeType: 'notification' | 'dday';
+>>>>>>> main
     count: number;
 }
 
@@ -9,6 +13,7 @@ export const NotificationBadge = ({
     badgeType,
     count
 }: NotificationBadgeProps) => {
+<<<<<<< HEAD
     const MAX_COUNT = 99;
     const BaseStyle =
         'inline-flex items-center justify-center text-center align-text-middle';
@@ -21,6 +26,15 @@ export const NotificationBadge = ({
         switch (badgeType) {
             case 'basic':
                 if (count > MAX_COUNT) {
+=======
+    const NotificationBadgeStyle =
+        'bg-red-600 px-2 py-1 rounded-full text-white font-semibold';
+
+    const NotificationBadgeContent = (type, count: number) => {
+        switch (type) {
+            case 'notification':
+                if (count > 99) {
+>>>>>>> main
                     return (
                         <>
                             <span style={{ position: 'relative', top: '-1px' }}>
@@ -49,13 +63,24 @@ export const NotificationBadge = ({
                     </>
                 );
             default:
+<<<<<<< HEAD
                 return null;
+=======
+                break;
+>>>>>>> main
         }
     };
 
     return (
+<<<<<<< HEAD
         <div className={`${BaseStyle} ${styleMap[badgeType]}`}>
             {renderContent(badgeType, count)}
+=======
+        <div
+            className={`${NotificationBadgeStyle} inline-flex items-center justify-center text-center align-text-middle`}
+        >
+            {NotificationBadgeContent(badgeType, count)}
+>>>>>>> main
         </div>
     );
 };
