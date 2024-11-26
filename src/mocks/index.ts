444b9996f6b20a5296c836c94f29432a521e7f1c
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-26 12:05:28 +0900

    refactor : index.ts 수정

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,17 +1,17 @@
-// 모킹 api 초기화, 실행
+async function setUpMocks() {
+    const { worker } = await import('./browser');
+    return worker.start({
+        onUnhandledRequest: 'bypass' // 모킹되지 않은 api는 그냥 통과
+    });
+}
+
 async function initMocks() {
     if (import.meta.env.MODE === 'development') {
-        const { worker } = await import('./browser');
-        return worker.start({
-            onUnhandledRequest: 'bypass' // 모킹되지 않은 api는 그냥 통과
-        });
+        setUpMocks();
     }
     return Promise.resolve();
 }
 
-// 개발 환경에서 실행됨...
-if (import.meta.env.MODE === 'development') {
-    initMocks();
-}
+initMocks().catch(console.error);
 
 export default initMocks;
