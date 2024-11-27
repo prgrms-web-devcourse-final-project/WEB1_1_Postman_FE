2adb537b804673f067339863aab708ee0c52a38b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-27 16:35:43 +0900

    feat : top bar 개발

---
+++ src/components/Common/TopBar/TopBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BackButton } from '../BackButton/BackButton';
+
+type TopBaerProps = { onClick: () => void };
+
+export const TopBar = ({ onClick }: TopBaerProps) => {
+    return (
+        <div className="flex items-center justify-between w-full px-4 text-2xl ">
+            <BackButton onClick={onClick} />
+            <p className="">완료</p>
+        </div>
+    );
+};
