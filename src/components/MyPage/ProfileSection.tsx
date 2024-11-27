042029592ace96907ebad443e0842f70e7ea8976
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-27 18:51:05 +0900

    chore : 괄호 오타 수정

--- src/components/MyPage/ProfileSection.tsx
+++ src/components/MyPage/ProfileSection.tsx
@@ -6,13 +6,15 @@ type ProfileSectionProps = {
     user: UserType;
 };
 
-export const ProfileSection = ({ user }: ProfileSectionProps) => (
-    <div className="flex flex-col gap-2">
-        <h2 className="font-bold text-gray-400">내 프로필 설정</h2>
-        <ListItem
-            image={user.profileImageUrl}
-            contents={user.nickname}
-            link={'/mypage/profile'}
-        />
-    </div>
-);
+export const ProfileSection = ({ user }: ProfileSectionProps) => {
+    return (
+        <div className="flex flex-col gap-2">
+            <h2 className="font-bold text-gray-400">내 프로필 설정</h2>
+            <ListItem
+                image={user.profileImageUrl}
+                contents={user.nickname}
+                link={'/mypage/profile'}
+            />
+        </div>
+    );
+};
