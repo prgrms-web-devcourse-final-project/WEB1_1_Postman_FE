258da5cdfdab51249524b929323d4db59b5f188e
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-27 18:50:20 +0900

    style : 2섹션 그리드 스타일 수정

--- src/components/MyPage/RecordGridSection.tsx
+++ src/components/MyPage/RecordGridSection.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const RecordGridSection = () => {
-    const recordStyle = `bg-gray-100 rounded-xl px-4 py-2 h-[110px]`;
-    const wideRecordStyle = `${recordStyle} col-span-2`;
+    const recordStyle = `w-full bg-gray-100 rounded-xl px-4 py-2 h-[110px]`;
+    const wideRecordStyle = `w-full bg-gray-100 rounded-xl px-4 py-2 h-[110px]`;
 
     return (
         <div className="flex flex-col gap-2">
             <h2 className="font-bold text-gray-400">내 기록</h2>
             <div className="grid grid-cols-2 gap-3 w-full max-w-sm">
-                <div className={recordStyle}>키워드 편지함</div>
-                <div className={recordStyle}>지도 편지함</div>
-                <div className={wideRecordStyle}>라벨첩</div>
+                <div className="col-span-1">
+                    <Link to="/mypage/letters/keyword" className="block">
+                        <div className={recordStyle}>키워드 편지함</div>
+                    </Link>
+                </div>
+                <div className="col-span-1">
+                    <Link to="/mypage/letters/map" className="block">
+                        <div className={recordStyle}>지도 편지함</div>
+                    </Link>
+                </div>
+                <div className="col-span-2">
+                    <Link to="/mypage/labels" className="block">
+                        <div className={wideRecordStyle}>라벨첩</div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
