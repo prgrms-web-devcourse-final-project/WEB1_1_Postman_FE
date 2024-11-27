4264bfc20c8b690cebaac6fbd57fd6d78fcb61d4
jaja <dkwjdals35@gmail.com>
2024-11-27 14:10:46 +0900

    feat : 디데이 컴포넌트 개발 완료

---
+++ src/components/Common/DayCounter/DayCounter.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DayCounter } from './DayCounter';
+
+const meta: Meta<typeof DayCounter> = {
+    component: DayCounter,
+    title: 'atoms/DayCounter',
+    tags: ['autodocs'],
+    argTypes: {}
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DayCounter>;
+
+export const Default: Story = {
+    args: {}
+};
