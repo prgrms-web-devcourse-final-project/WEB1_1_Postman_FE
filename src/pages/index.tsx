ded7f8c73351d09b4132a2f5d8aea492aa941126
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-28 17:29:28 +0900

    chore : 경로 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { HomePage } from '@/pages/HomePage';
 import { CreateLetterPage } from '@/pages/Letter/Create/CreateLetterPage';
 import { MapExplorerPage } from '@/pages/MapExplorerPage';
 import { MyPage } from '@/pages/User/MyPage/Mypage';
-import { LoginPage } from '@/pages/LoginPage';
+import { LoginPage } from '@/pages/User/Login/LoginPage';
 import { RegisterPage } from '@/pages/RegisterPage';
 import { ArchivedPage } from '@/pages/ArchivedPage';
 import { LabelCollectionsPage } from '@/pages/LabelCollectionsPage';
