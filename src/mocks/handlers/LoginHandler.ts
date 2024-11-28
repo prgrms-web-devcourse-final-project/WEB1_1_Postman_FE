0ba7d7780eabb070ef6d1d47d5bd2f2592683b2e
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-28 17:48:55 +0900

    fix : login 핸들러 누락된 status 추가

--- src/mocks/handlers/LoginHandler.ts
+++ src/mocks/handlers/LoginHandler.ts
@@ -18,7 +18,7 @@ export const LoginHandler = [
         '*/auth/signin',
         async ({ request }) => {
             const { email } = await request.json();
-
+            console.log(email);
             // 로그인 성공
             if (email === 'success@email.com') {
                 return HttpResponse.json(
@@ -33,6 +33,9 @@ export const LoginHandler = [
                         }
                     },
                     {
+                        status: 200
+                    },
+                    {
                         headers: {
                             'Content-Type': 'application/json',
                             'Set-Cookie':
@@ -44,31 +47,46 @@ export const LoginHandler = [
 
             // 존재하지 않는 유저
             if (email === 'notfound@email.com') {
-                return HttpResponse.json({
-                    code: 400,
-                    status: 'USER_NOT_FOUND',
-                    message: '존재하지 않는 유저',
-                    data: null
-                });
+                return HttpResponse.json(
+                    {
+                        code: 400,
+                        status: 'USER_NOT_FOUND',
+                        message: '존재하지 않는 유저',
+                        data: null
+                    },
+                    {
+                        status: 400
+                    }
+                );
             }
 
             // 오류
             if (email === 'error@email.com') {
-                return HttpResponse.json({
-                    code: 500,
-                    status: 'INTERNER_SERVER_ERROR',
-                    message: '로그인 실패',
-                    data: null
-                });
+                return HttpResponse.json(
+                    {
+                        code: 500,
+                        status: 'INTERNER_SERVER_ERROR',
+                        message: '로그인 실패',
+                        data: null
+                    },
+                    {
+                        status: 500
+                    }
+                );
             }
 
             // 기본 에러
-            return HttpResponse.json({
-                code: 400,
-                status: 'BAD_REQUEST',
-                message: '잘못된 요청입니다',
-                data: null
-            });
+            return HttpResponse.json(
+                {
+                    code: 400,
+                    status: 'BAD_REQUEST',
+                    message: '잘못된 요청입니다',
+                    data: null
+                },
+                {
+                    status: 400
+                }
+            );
         }
     )
 ];
