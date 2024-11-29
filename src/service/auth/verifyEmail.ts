163e52ef54dec7ae8cace16a4cbbc47d836c4784
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-29 17:39:38 +0900

    feat : 이메일 인증 로직 추가

---
+++ src/service/auth/verifyEmail.ts
@@ -0,0 +1,33 @@
+import { AxiosError } from 'axios';
+import { defaultApi } from '@/service/api';
+import { ApiResponse } from '@/types/apiResponse';
+
+type VerifyEmailProps = {
+    email: string;
+    authNum: string;
+};
+
+type VerifyEmailResponse = ApiResponse<'success' | 'fail'>;
+
+export async function verifyEmail({
+    email,
+    authNum
+}: VerifyEmailProps): Promise<VerifyEmailResponse> {
+    const api = defaultApi();
+    try {
+        const response = await api.post('/auth/email/verify', {
+            email,
+            authNum
+        });
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response?.data) {
+            throw error.response.data;
+        }
+        throw {
+            code: 500,
+            status: 'INTERNAL_SERVER_ERROR',
+            message: '서버와의 통신에 실패했습니다.'
+        };
+    }
+}
