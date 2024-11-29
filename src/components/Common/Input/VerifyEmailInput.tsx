d457a0c4136e5222bd6b21fafa596ce0bd23f1d8
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-29 23:38:24 +0900

    chore : VerifyEmailInput으로 컴포넌트 파일명 수정

--- src/components/Common/Input/VerifyEmailInput.tsx
+++ src/components/Common/Input/VerifyEmailInput.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import { AUTH_INPUT_VALIDATION } from '@/constants/authInputValidation';
 import { Input } from './Input';
 
-type EmailVerifyInputProps = {
+type VerifyEmailInputProps = {
     defaultValue?: string;
     disabled?: boolean;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onValueChange?: (value: string) => void;
 };
 
-export const EmailVerifyInput = ({
+export const VerifyEmailInput = ({
     defaultValue,
-    disabled
-}: EmailVerifyInputProps) => {
+    disabled,
+    onValueChange
+}: VerifyEmailInputProps) => {
     return (
         <Input
             autoComplete="off"
             text="이메일 인증번호"
-            name="emailVerifyCode"
-            type="emailVerifyCode"
+            name="verifyEmailCode"
+            type="verifyEmailCode"
             errorMessage={AUTH_INPUT_VALIDATION.emailVerifyCode.errorMessage}
             pattern={AUTH_INPUT_VALIDATION.emailVerifyCode.regexp}
             required
             togglePassword={false}
             defaultValue={defaultValue}
             disabled={disabled}
+            onValueChange={onValueChange}
         />
     );
 };
