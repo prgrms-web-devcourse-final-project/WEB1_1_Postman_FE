f1d29dfe804e9918758765fd04369bf3688b7217
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-11-29 17:39:27 +0900

    feat : sendEmail 로직 추가

---
+++ src/service/auth/sendEmail.ts
@@ -0,0 +1,29 @@
+import { AxiosError } from 'axios';
+import { defaultApi } from '@/service/api';
+import { ApiResponse } from '@/types/apiResponse';
+
+type SendEmailProps = {
+    email: string;
+};
+
+type SendEmailResponse = ApiResponse<'success' | 'fail'>;
+
+export async function sendEmail({
+    email
+}: SendEmailProps): Promise<SendEmailResponse> {
+    const api = defaultApi();
+
+    try {
+        const response = await api.post('/auth/email/send-email', { email });
+        return response.data;
+    } catch (error) {
+        if (error instanceof AxiosError && error.response?.data) {
+            throw error.response.data;
+        }
+        throw {
+            code: 500,
+            status: 'INTERNAL_SERVER_ERROR',
+            message: '서버와의 통신에 실패했습니다.'
+        };
+    }
+}
