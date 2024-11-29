b3e4aab3cad779690a1c527c853d2d160394c326
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-29 11:54:17 +0900

    feat  : 편지 작성 페이지 개발  (#128)
    * env : react-spring-bottom-sheet - > alpha 버전으로 마이그레이션
    
    * env : 필요 의존성 설치
    
    * chore : 하위 컴포넌트 애니메이션 수정
    
    * feat : form 개발
    
    * feat : 폰트, 이미지 선택 기능
    
    * refactor : 페이지 분리

--- src/pages/Letter/Create/CreateLetterPage.tsx
+++ src/pages/Letter/Create/CreateLetterPage.tsx
@@ -1,7 +1,16 @@
+import { TopBar } from '@/components/Common/TopBar/TopBar';
+import { PostLetterForm } from '@/components/Letter/Post/PostLetterForm';
 import React from 'react';
 
 export const CreateLetterPage = () => {
     return (
-        <div className="bg-slate-500 w-[375px] max-w-[768px] h-[815px]"></div>
+        <div className=" w-[375px] max-w-[768px] h-[815px]">
+            <TopBar
+                onClick={() => {
+                    alert('back~');
+                }}
+            />
+            <PostLetterForm />
+        </div>
     );
 };
