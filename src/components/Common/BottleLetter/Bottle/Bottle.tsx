1b71a2168871cf532a334d525b9c0e49769f84cf
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-01 02:32:14 +0900

    chore : bottle 애니메이션 위치 수정

--- src/components/Common/BottleLetter/Bottle/Bottle.tsx
+++ src/components/Common/BottleLetter/Bottle/Bottle.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export const Bottle = () => {
     return (
-        <div className="flex justify-center animate-wiggle animate-infinite animate-ease-linear ">
-            <img src="/유리병_샘플.png" className="object-contain w-24 h-52" />
+        <div className="flex justify-center ">
+            <img
+                src="/유리병_샘플.png"
+                className="object-contain w-24 h-52 animate-wiggle animate-infinite animate-ease-linear"
+            />
         </div>
     );
 };
