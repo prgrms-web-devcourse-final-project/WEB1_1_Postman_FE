ad8f3f0238d26d19452c3ed63a6548d778eaaa9d
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-01 15:38:44 +0900

    feat : 회원가입 관련 허용되는 문자열 변경

--- src/constants/authInputValidation.ts
+++ src/constants/authInputValidation.ts
@@ -1,13 +1,11 @@
 export const AUTH_INPUT_VALIDATION = {
     nickname: {
-        regexp: /^[a-zA-Z0-9._%+-]{5,15}$/,
-        errorMessage:
-            '닉네임은 5자 이상 15자 이하여야 하며 영문자, 숫자, 특수문자(._%+-)만 사용해야 합니다.'
+        regexp: /^[a-zA-Z0-9가-힣]{1,10}$/,
+        errorMessage: '닉네임은 1-10자 사이의 한글, 영문, 숫자만 가능합니다.'
     },
     password: {
         regexp: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/,
-        errorMessage:
-            '비밀번호는 8자 이상이어야 하며, 영문과 숫자로만 이루어져야 합니다.'
+        errorMessage: '비밀번호는 8자 이상의 영문과 숫자로 작성해주세요.'
     },
     email: {
         regexp: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
