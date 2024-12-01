e615a5047065be061f636b03a193d26f78302630
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-01 20:00:55 +0900

    feat : [임시] 정규표현식 에러 메세지 간결화

--- src/constants/authInputValidation.ts
+++ src/constants/authInputValidation.ts
@@ -1,18 +1,18 @@
 export const AUTH_INPUT_VALIDATION = {
     nickname: {
-        regexp: /^[a-zA-Z0-9가-힣]{1,10}$/,
-        errorMessage: '닉네임은 1-10자 사이의 한글, 영문, 숫자만 가능합니다.'
+        regexp: /^[a-zA-Z0-9가-힣]{1,8}$/,
+        errorMessage: '8자 이내의 한글, 영문, 숫자'
     },
     password: {
         regexp: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/,
-        errorMessage: '비밀번호는 8자 이상의 영문과 숫자로 작성해주세요.'
+        errorMessage: '8자 이상의 영문, 숫자'
     },
     email: {
         regexp: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        errorMessage: '올바른 이메일 형식을 입력해주세요.'
+        errorMessage: '잘못된 형식입니다.'
     },
     emailVerifyCode: {
         regexp: /^[A-Za-z0-9]{6}$/,
-        errorMessage: '올바른 인증 코드를 입력해주세요.'
+        errorMessage: '잘못된 형식입니다.'
     }
 };
