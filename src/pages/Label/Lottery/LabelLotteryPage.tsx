39015d9a3d8e4e7c23de005c50c3a61aa64d5476
kangminguu <mingudat9909@gmail.com>
2024-12-02 09:55:35 +0900

    feat : 라벨뽑기 페이지 레이아웃 구성

--- src/pages/Label/Lottery/LabelLotteryPage.tsx
+++ src/pages/Label/Lottery/LabelLotteryPage.tsx
@@ -1,3 +1,24 @@
+import { Margin } from '@/components/Common/Margin/Margin';
+
 export const LabelLotteryPage = () => {
-    return <div className="">LabelLotteryPage</div>;
+    return (
+        <div className="h-[100vh] flex-center flex-col gap-[10px]">
+            <div className="bg-slate-500 h-[200px] w-full px-[50px]"></div>
+
+            <Margin top={30} />
+
+            <p>뽑기 이벤트</p>
+            <p>라벨 프로필사진 뽑아보세요</p>
+
+            <Margin top={50} />
+
+            <div>
+                <button className="btn-base w-[240px] h-[60px]">뽑기</button>
+                <Margin top={10} />
+                <button className="btn-base w-[240px] h-[60px]">
+                    친구공유
+                </button>
+            </div>
+        </div>
+    );
 };
