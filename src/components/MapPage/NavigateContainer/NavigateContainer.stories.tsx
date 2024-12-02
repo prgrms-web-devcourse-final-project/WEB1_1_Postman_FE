1926d10ef2c609bbd57214db70c9cb6d2e78c074
jaja <dkwjdals35@gmail.com>
2024-12-02 16:09:12 +0900

    chore :NavigateContainer 컴포넌트 수정

--- src/components/MapPage/NavigateContainer/NavigateContainer.stories.tsx
+++ src/components/MapPage/NavigateContainer/NavigateContainer.stories.tsx
@@ -14,11 +14,6 @@ type Story = StoryObj<typeof NavigateContainer>;
 
 export const Default: Story = {
     args: {
-        title: '가을 바람',
-        distance: '15km',
-        count: 4,
-        clickEvent: () => {
-            alert('헬로');
-        }
+        count: 4
     }
 };
