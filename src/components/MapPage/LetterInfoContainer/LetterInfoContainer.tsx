db65a62b7295fabe3b6dffeae848b711447a5c2c
jaja <dkwjdals35@gmail.com>
2024-12-02 16:06:22 +0900

    chore : LetterInfoContainer 컴포넌트 수정

--- src/components/MapPage/LetterInfoContainer/LetterInfoContainer.tsx
+++ src/components/MapPage/LetterInfoContainer/LetterInfoContainer.tsx
@@ -1,17 +1,21 @@
+import { DayCounter } from '@/components/Common/DayCounter/DayCounter';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 interface LetterInfoContainerProps {
+    id: number;
     title: string;
-    keyword: string;
+    distance: number;
     date: string;
-    clickEvent: () => void;
+    daysLeft: number;
 }
 
 export const LetterInfoContainer = ({
+    id,
     title,
-    keyword,
+    distance,
     date,
-    clickEvent
+    daysLeft
 }: LetterInfoContainerProps) => {
     return (
         <div className="flex flex-col w-[330px] bg-gray-200 rounded-lg p-4 shadow-md">
@@ -19,19 +23,25 @@ export const LetterInfoContainer = ({
                 <div className="p-2 bg-gray-300 rounded-md">
                     <img src="/bottle.png" alt="Bottle" className="w-20 h-20" />
                 </div>
-                <div className="flex flex-col justify-between h-20">
+                <div className="flex flex-col h-20">
+                    <div className="flex-center gap-2">
+                        <p className="text-sm text-gray-600">작성일: {date}</p>
+                        <DayCounter width="40px" daysLeft={daysLeft} />
+                    </div>
+
                     <h3 className="text-lg font-semibold">{title}</h3>
-                    <p className="text-sm text-gray-600">키워드: {keyword}</p>
-                    <p className="text-sm text-gray-600">작성일: {date}</p>
+                    <p className="text-sm text-gray-600">
+                        현재 위치에서 {distance}m
+                    </p>
                 </div>
             </div>
 
-            <button
+            <NavLink
+                to={`/letter/map/:${id}`}
                 className="w-full py-2 text-center text-gray-700 bg-gray-300 rounded-lg hover:bg-gray-400"
-                onClick={clickEvent}
             >
                 편지 보러가기
-            </button>
+            </NavLink>
         </div>
     );
 };
