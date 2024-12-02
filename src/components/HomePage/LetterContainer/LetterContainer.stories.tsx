0a25905afab8984de12088c7b7308a7810a4994e
jaja <dkwjdals35@gmail.com>
2024-12-02 17:36:48 +0900

    chore :  LetterContainer, MapLetterDetail 스토리북 오류 수정

--- src/components/HomePage/LetterContainer/LetterContainer.stories.tsx
+++ src/components/HomePage/LetterContainer/LetterContainer.stories.tsx
@@ -1,11 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { LetterContainer } from './LetterContainer';
+import { MemoryRouter } from 'react-router-dom';
 
 const meta: Meta<typeof LetterContainer> = {
     component: LetterContainer,
     title: 'molecule/LetterContainer',
     tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <MemoryRouter>
+                <Story />
+            </MemoryRouter>
+        )
+    ],
     argTypes: {},
     parameters: {
         docs: {
