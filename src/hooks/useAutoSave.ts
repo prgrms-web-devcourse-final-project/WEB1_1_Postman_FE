3da2620053f789da9f49d0b083879fa6789483cb
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-04 09:17:17 +0900

    feat : 편지 작성 자동 저장 기능 추가 (#206)
    * feat : useAutoSave 훅 개발
    
    * feat : useAutoSave를 이용한 자동 저장

---
+++ src/hooks/useAutoSave.ts
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useToastStore } from './useToastStore';
+
+type AutoSaveCallback = () => void;
+
+const useAutoSave = (callback: AutoSaveCallback, interval: number) => {
+    const { addToast } = useToastStore();
+
+    useEffect(() => {
+        const autoSave = setInterval(() => {
+            setTimeout(() => {
+                try {
+                    callback();
+                    addToast('임시저장에 성공했습니다.', 'success');
+                } catch (error) {
+                    addToast('임시저장에 실패했습니다.', 'error');
+                    console.error(error);
+                }
+            }, 0);
+        }, interval);
+
+        return () => {
+            clearInterval(autoSave);
+        };
+    }, [callback, interval, addToast]);
+};
+
+export default useAutoSave;
