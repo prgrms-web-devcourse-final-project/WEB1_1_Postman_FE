9bcfa19b19328b202ff75da8e02055f6f9c0bb6a
kangminguu <mingudat9909@gmail.com>
2024-12-04 10:15:34 +0900

    style : Toggle 컴포넌트 스타일 수정

--- src/components/Common/Toggle/Toggle.tsx
+++ src/components/Common/Toggle/Toggle.tsx
@@ -12,28 +12,26 @@ export const Toggle = ({
     rightLabel
 }: ToggleProps) => {
     return (
-        <div className="relative flex items-center justify-between w-[140px] h-[30px] bg-gray-200 rounded-full p-1">
+        <div className="relative flex items-center w-fit h-[30px] bg-[#F7FBFE] rounded-full border border-[rgba(34,184,239,0.13)]">
             <div
                 onClick={onToggle}
-                className={`absolute w-[78px] h-[30px] bg-gray-100 rounded-full cursor-pointer transition-all duration-200 ease-in-out ${
-                    isChecked ? 'translate-x-[60px]' : '-translate-x-2'
+                className={`absolute w-1/2 left-0 h-[30px] bg-[rgb(34,184,239)] rounded-full cursor-pointer transition-all duration-200 ease-in-out ${
+                    isChecked ? 'translate-x-full' : 'translate-x-0'
                 }`}
             />
             <div
                 onClick={onToggle}
-                className={`absolute left-5 font-bold text-sm transform -translate-y-1/2 ${
-                    isChecked ? 'text-gray-400' : 'text-black'
+                className={`px-4 text-sm w-1/2 transform ${
+                    isChecked ? 'text-gray-400' : 'text-white'
                 }`}
-                style={{ top: '50%' }}
             >
                 {leftLabel}
             </div>
             <div
                 onClick={onToggle}
-                className={`absolute right-5 font-bold text-sm transform -translate-y-1/2 ${
-                    isChecked ? 'text-black' : 'text-gray-400'
+                className={`px-4 text-sm w-1/2 transform ${
+                    isChecked ? 'text-white' : 'text-gray-400'
                 }`}
-                style={{ top: '50%' }}
             >
                 {rightLabel}
             </div>
