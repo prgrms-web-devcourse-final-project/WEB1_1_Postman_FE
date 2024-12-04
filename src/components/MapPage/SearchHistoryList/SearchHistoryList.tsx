a689e53696a863e18d23a520e61dba85461c1078
jaja <dkwjdals35@gmail.com>
2024-12-04 21:54:47 +0900

    feat: 지도 편지 페이지 최근 검색어 클릭 시 검색 기능

--- src/components/MapPage/SearchHistoryList/SearchHistoryList.tsx
+++ src/components/MapPage/SearchHistoryList/SearchHistoryList.tsx
@@ -4,20 +4,33 @@ type SearchHistoryListProps = {
     date: string;
     index: number;
     onDelete: (index: number) => void;
+    onClick: (place: string) => void;
 };
 
 export const SearchHistoryList = ({
     place,
     date,
     index,
-    onDelete
+    onDelete,
+    onClick
 }: SearchHistoryListProps) => {
     return (
-        <div className="flex items-center py-2 justify-between w-full text-gray-400">
+        <div
+            className="flex items-center py-2 px-4 justify-between w-full cursor-pointer text-gray-400 hover:bg-slate-200"
+            onClick={() => {
+                onClick(place);
+            }}
+        >
             <LuMapPin />
             <span className="flex-1">{place}</span>
             <span className="mr-2">{date}</span>
-            <span className="cursor-pointer" onClick={() => onDelete(index)}>
+            <span
+                className="cursor-pointer"
+                onClick={(e) => {
+                    e.stopPropagation();
+                    onDelete(index);
+                }}
+            >
                 x
             </span>
         </div>
