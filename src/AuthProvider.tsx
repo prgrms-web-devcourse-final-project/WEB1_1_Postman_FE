54945c93e6a447c079c8ab8c982c29b30e180b81
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-06 10:42:00 +0900

    feat : AuthProvider 작성

---
+++ src/AuthProvider.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, useEffect, useState } from 'react';
+import { tokenStorage } from './service/auth/tokenStorage';
+import { useUserInfo } from './hooks/useUserInfo';
+
+interface AuthLayoutProps {
+    children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthLayoutProps) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const { handleGetUserInfo } = useUserInfo();
+
+    useEffect(() => {
+        const initializeAuth = async () => {
+            const accessToken = tokenStorage.getAccessToken();
+            if (accessToken) {
+                const success = await handleGetUserInfo();
+                if (success) setIsLoading(false);
+            }
+        };
+
+        initializeAuth();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return children;
+};
