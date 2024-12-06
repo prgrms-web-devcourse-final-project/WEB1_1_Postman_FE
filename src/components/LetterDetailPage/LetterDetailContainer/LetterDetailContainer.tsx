4ab73705eae197f04ea432de7a68d03c101cc376
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-07 05:12:49 +0900

    chore : 편지 상세 보기 수정

--- src/components/LetterDetailPage/LetterDetailContainer/LetterDetailContainer.tsx
+++ src/components/LetterDetailPage/LetterDetailContainer/LetterDetailContainer.tsx
@@ -9,6 +9,8 @@ import { useKeywordLetterDetail } from '@/hooks/useGetKeywordLetterDetail';
 import { MapLetterDetail } from '../LetterDatail/MapLetterDetail';
 import { KeywordLetterDetail } from '../LetterDatail/KeywordLetterDetail';
 import { ReplyList } from '../ReplyList/ReplyList';
+import { ThemeWrapper } from '@/components/CreatLetterPage/ThemeWrapper/ThemeWrapper';
+import { TopBar } from '@/components/Common/TopBar/TopBar';
 
 type LetterDetailContainerProps = {
     hasReplies?: boolean;
@@ -40,11 +42,6 @@ export const LetterDetailContainer = ({
         return <div>Error: {error.message}</div>;
     }
 
-    const imageItem = {
-        id: '편지지_샘플_1',
-        name: '이미지',
-        src: '/편지지_샘플_1.png'
-    };
     const labelItem = {
         id: '라벨_샘플',
         name: '이미지',
@@ -58,69 +55,64 @@ export const LetterDetailContainer = ({
     ];
     return (
         <>
-            <div className="relative mx-auto mt-4 max-w">
-                <div className="mx-auto w-[710px]">
-                    <BackButton onClick={onBackClick} />
-                </div>
-                {letterId && (
-                    <div className="absolute top-0 flex mt-10 right-8">
-                        <DeleteButton id={letterId} />
-                        {!data?.isOwner && <ReportButton id={letterId} />}
-                    </div>
-                )}
-                <div className="relative mt-16 flex-center">
-                    <img
-                        src={imageItem.src}
-                        alt={imageItem.name}
-                        className="w-[710px] h-[900px] relative"
-                    />
-                    <img
-                        src={labelItem.src}
-                        alt={labelItem.name}
-                        className="absolute top-4 translate-x-40 w-[125.32px] h-[201.1px]"
-                    />
-                    {type === 'map' ? (
-                        <MapLetterDetail
-                            title="편지제목"
-                            content="편지내용"
-                            date="24.11.18"
-                            place="서울시 종로구 평창동"
-                            hint="서대문역 앞 붕어빵 가게에서"
-                        />
-                    ) : (
-                        data && <KeywordLetterDetail letterData={data} />
+            <TopBar />
+            <ThemeWrapper themeId={Number(data!.paper)}>
+                <div className="relative mx-auto mt-4 max-w">
+                    {letterId && (
+                        <div className="absolute top-0 flex mt-10 right-8">
+                            <DeleteButton id={letterId} />
+                            {!data?.isOwner && <ReportButton id={letterId} />}
+                        </div>
                     )}
-                </div>
-            </div>
-
-            {data?.isOwner ? (
-                hasReplies ? (
-                    <div className="mt-16 w-[710px]  mx-auto">
-                        <ReplyList replies={sampleReplies} />
+                    <div className="relative mt-16 flex-center">
+                        <img
+                            src={labelItem.src}
+                            alt={labelItem.name}
+                            className="absolute top-4 translate-x-40 w-[125.32px] h-[201.1px]"
+                        />
+                        {type === 'map' ? (
+                            <MapLetterDetail
+                                title="편지제목"
+                                content="편지내용"
+                                date="24.11.18"
+                                place="서울시 종로구 평창동"
+                                hint="서대문역 앞 붕어빵 가게에서"
+                            />
+                        ) : (
+                            data && <KeywordLetterDetail letterData={data} />
+                        )}
                     </div>
-                ) : null
-            ) : type === 'map' ? (
-                <div className="gap-4 mx-auto mt-4 flex-center max-w">
-                    {!data?.isOwner && (
-                        <>
-                            <button className="btn-base rounded-3xl w-[339.82px] h-[80px]">
-                                보관하기
-                            </button>
-                            <button className="btn-base rounded-3xl w-[339.82px] h-[80px]">
+                </div>
+
+                {data?.isOwner ? (
+                    hasReplies ? (
+                        <div className="mt-16 w-[710px]  mx-auto">
+                            <ReplyList replies={sampleReplies} />
+                        </div>
+                    ) : null
+                ) : type === 'map' ? (
+                    <div className="gap-4 mx-auto mt-4 flex-center max-w">
+                        {!data?.isOwner && (
+                            <>
+                                <button className="btn-base rounded-3xl w-[339.82px] h-[80px]">
+                                    보관하기
+                                </button>
+                                <button className="btn-base rounded-3xl w-[339.82px] h-[80px]">
+                                    편지에 답장하기
+                                </button>
+                            </>
+                        )}
+                    </div>
+                ) : (
+                    <div className="gap-4 mx-auto mt-4 flex-center max-w">
+                        {!data?.isOwner && (
+                            <button className="btn-base rounded-3xl w-[700px] h-[80px]">
                                 편지에 답장하기
                             </button>
-                        </>
-                    )}
-                </div>
-            ) : (
-                <div className="gap-4 mx-auto mt-4 flex-center max-w">
-                    {!data?.isOwner && (
-                        <button className="btn-base rounded-3xl w-[700px] h-[80px]">
-                            편지에 답장하기
-                        </button>
-                    )}
-                </div>
-            )}
+                        )}
+                    </div>
+                )}
+            </ThemeWrapper>
         </>
     );
 };
