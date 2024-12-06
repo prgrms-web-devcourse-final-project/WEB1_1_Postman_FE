12014ab615708843e58aef0ad44720f25dc40a3c
jaja <dkwjdals35@gmail.com>
2024-12-06 21:13:16 +0900

    feat : 주변 편지 상세 조회 데이터 바인딩

--- src/components/LetterDetailPage/LetterDatail/MapLetterDetail.tsx
+++ src/components/LetterDetailPage/LetterDatail/MapLetterDetail.tsx
@@ -1,45 +1,41 @@
-import { ProfileImage } from '@/components/Common/ProfileImage/ProfileImage';
 import { DayCounter } from '@/components/Common/DayCounter/DayCounter';
-import { LuMapPin } from 'react-icons/lu';
+import { formatDate } from '@/util/formatDate';
+
 type MapLetterDetailProps = {
-    title: string;
-    content: string;
-    date: string;
-    place: string;
-    hint: string;
+    letterData: {
+        title: string;
+        content: string;
+        description: string;
+        createdAt: string;
+        profileImg: string;
+        font: string;
+        paper: string;
+        label: string;
+        isOwner: boolean;
+    };
 };
 
-export const MapLetterDetail = ({
-    title,
-    content,
-    date,
-    place,
-    hint
-}: MapLetterDetailProps) => {
+export const MapLetterDetail = ({ letterData }: MapLetterDetailProps) => {
+    console.log(letterData);
+    const { title, content, description, createdAt, profileImg } = letterData;
     return (
         <>
-            <div className="absolute bottom-4 translate-x-40">
-                <ProfileImage
-                    imageItem={{ id: '123', name: '가오리', src: '/라밸_샘플' }}
-                    width="50px"
-                    height="50px"
-                />
-            </div>
-            <p className="absolute left-24 top-60 font-bold">{title}</p>
-            <p className="absolute left-24 top-[19rem]">{content}</p>
-            <div className="absolute bottom-48 flex items-center left-24">
-                <LuMapPin /> {place}
-            </div>
-
-            <div className="absolute bottom-20 rounded-3xl w-[580px] h-[100px] bg-gray-300">
-                <p className="font-bold m-4">장소 힌트</p>
-                <p className="overflow-hidden whitespace-nowrap text-ellipsis m-4">
-                    {hint}
-                </p>
-            </div>
-            <div className="absolute bottom-4 translate-x-60 flex-col">
-                <p className="ml-2">{date}</p>
-                <DayCounter daysLeft={21} width="70px" height="20px" />
+            <p className="absolute top-[15rem] font-bold">{title}</p>
+            <p className="absolute top-[30rem]">{content}</p>
+            <div className="absolute bottom-16  min-w-[375px] max-w-[477px]  h-[100px]">
+                <div className="absolute bottom-20 rounded-3xl w-full h-[100px] bg-gray-300">
+                    <p className="font-bold m-4">장소 힌트</p>
+                    <p className="overflow-hidden whitespace-nowrap text-ellipsis m-4">
+                        {description}
+                    </p>
+                </div>
+                <div className="absolute bottom-4 translate-x-60 flex-col">
+                    <div className="absolute bottom-4 translate-x-40">
+                        {profileImg}
+                    </div>
+                    <p className="ml-2">{formatDate(createdAt)}</p>
+                    <DayCounter daysLeft={21} width="70px" height="20px" />
+                </div>
             </div>
         </>
     );
--- src/components/LetterDetailPage/LetterDetailContainer/LetterDetailContainer.tsx
+++ src/components/LetterDetailPage/LetterDetailContainer/LetterDetailContainer.tsx
@@ -1,14 +1,13 @@
 import { BackButton } from '@/components/Common/BackButton/BackButton';
 import { useNavigate } from 'react-router-dom';
-
 import { useParams } from 'react-router-dom';
 import { DeleteButton } from '@/components/LetterDetailPage/Delete/DeleteButton';
-
 import { ReportButton } from '@/components/LetterDetailPage/Report/ReportButton';
 import { useKeywordLetterDetail } from '@/hooks/useGetKeywordLetterDetail';
 import { MapLetterDetail } from '../LetterDatail/MapLetterDetail';
 import { KeywordLetterDetail } from '../LetterDatail/KeywordLetterDetail';
 import { ReplyList } from '../ReplyList/ReplyList';
+import { useNearbyLettersDetail } from '@/hooks/useGetNearbyLettersDetail';
 
 type LetterDetailContainerProps = {
     hasReplies?: boolean;
@@ -17,28 +16,53 @@ type LetterDetailContainerProps = {
 export const LetterDetailContainer = ({
     hasReplies
 }: LetterDetailContainerProps) => {
-    const { type, letterId } = useParams<{
+    const { type, letterId, lat, lot } = useParams<{
         type: 'map' | 'keyword';
         letterId: string;
+        lat: string;
+        lot: string;
     }>();
-    const { data, isLoading, error } = useKeywordLetterDetail({
-        letterId: letterId || ''
+    console.log('Type:', type, 'Letter ID:', letterId);
+    const navigate = useNavigate();
+
+    const isMapType = type === 'map';
+
+    const {
+        data: keywordData,
+        isLoading: isKeywordLoading,
+        error: keywordError
+    } = useKeywordLetterDetail({
+        letterId: !isMapType ? letterId || '' : ''
     });
 
-    const navigate = useNavigate();
+    const {
+        data: mapData,
+        isLoading: isMapLoading,
+        error: mapError
+    } = useNearbyLettersDetail({
+        letterId: letterId ? Number(letterId) || 0 : 0,
+        longitude: lot || '',
+        latitude: lat || ''
+    });
+
+    console.log(mapData);
+
+    if (isKeywordLoading || isMapLoading) {
+        return <div>Loading...</div>;
+    }
+    if (!isMapType && keywordError instanceof Error) {
+        console.error('Keyword Error:', keywordError.message);
+        return <div>Error: 키워드 편지가 존재하지 않습니다.</div>;
+    }
+
+    if (isMapType && mapError instanceof Error) {
+        console.error('Map Error:', mapError.message);
+        return <div>Error: 지도 데이터를 가져오는 중 문제가 발생했습니다.</div>;
+    }
 
     const onBackClick = () => {
         navigate(-1);
     };
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error instanceof Error) {
-        console.error('error:', error.message);
-        return <div>Error: {error.message}</div>;
-    }
 
     const imageItem = {
         id: '편지지_샘플_1',
@@ -56,6 +80,7 @@ export const LetterDetailContainer = ({
         { id: 2, title: '답장 제목 2', date: '24.11.29' },
         { id: 3, title: '답장 제목 3', date: '24.11.30' }
     ];
+
     return (
         <>
             <div className="relative mx-auto mt-4 max-w">
@@ -65,7 +90,9 @@ export const LetterDetailContainer = ({
                 {letterId && (
                     <div className="absolute top-0 flex mt-10 right-8">
                         <DeleteButton id={letterId} />
-                        {!data?.isOwner && <ReportButton id={letterId} />}
+                        {!keywordData?.isOwner && (
+                            <ReportButton id={letterId} />
+                        )}
                     </div>
                 )}
                 <div className="relative mt-16 flex-center">
@@ -79,29 +106,25 @@ export const LetterDetailContainer = ({
                         alt={labelItem.name}
                         className="absolute top-4 translate-x-40 w-[125.32px] h-[201.1px]"
                     />
-                    {type === 'map' ? (
-                        <MapLetterDetail
-                            title="편지제목"
-                            content="편지내용"
-                            date="24.11.18"
-                            place="서울시 종로구 평창동"
-                            hint="서대문역 앞 붕어빵 가게에서"
-                        />
+                    {isMapType && mapData ? (
+                        <MapLetterDetail letterData={mapData} />
+                    ) : keywordData ? (
+                        <KeywordLetterDetail letterData={keywordData} />
                     ) : (
-                        data && <KeywordLetterDetail letterData={data} />
+                        <div>Error: 편지가 존재하지 않습니다.</div>
                     )}
                 </div>
             </div>
 
-            {data?.isOwner ? (
+            {keywordData?.isOwner ? (
                 hasReplies ? (
-                    <div className="mt-16 w-[710px]  mx-auto">
+                    <div className="mt-16 w-[710px] mx-auto">
                         <ReplyList replies={sampleReplies} />
                     </div>
                 ) : null
-            ) : type === 'map' ? (
+            ) : isMapType ? (
                 <div className="gap-4 mx-auto mt-4 flex-center max-w">
-                    {!data?.isOwner && (
+                    {!mapData?.isOwner && (
                         <>
                             <button className="btn-base rounded-3xl w-[339.82px] h-[80px]">
                                 보관하기
@@ -114,7 +137,7 @@ export const LetterDetailContainer = ({
                 </div>
             ) : (
                 <div className="gap-4 mx-auto mt-4 flex-center max-w">
-                    {!data?.isOwner && (
+                    {!keywordData?.isOwner && (
                         <button className="btn-base rounded-3xl w-[700px] h-[80px]">
                             편지에 답장하기
                         </button>
