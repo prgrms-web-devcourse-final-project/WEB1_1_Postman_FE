f0897b06f012e6cb0991c66ed4a176de05b69358
kangminguu <mingudat9909@gmail.com>
2024-12-06 12:33:54 +0900

    chore : 사용자 키워드 등록 성공 시 콘솔 찍히던거 삭제

--- src/hooks/useCreateUserKeywords.ts
+++ src/hooks/useCreateUserKeywords.ts
@@ -4,9 +4,7 @@ import { createUserKeyword } from '@/service/keyword/createUserKeyword';
 export const useCreateUserKeywords = () => {
     const mutation = useMutation({
         mutationFn: createUserKeyword,
-        onSuccess: (data) => {
-            console.log('키워드 생성 성공:', data);
-        },
+        onSuccess: () => {},
         onError: (error) => {
             console.error('키워드 생성 실패:', error);
         }
