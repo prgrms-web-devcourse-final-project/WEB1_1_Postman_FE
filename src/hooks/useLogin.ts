1ed75bfdb7908a4aefa4c289f18368f30e967af2
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-06 10:42:52 +0900

    chore : 더미데이터 바인딩 해제

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,39 +1,23 @@
 import { useNavigate } from 'react-router-dom';
-import { useUserStore } from '@/stores/useUserStore';
 import { useToastStore } from '@/hooks/useToastStore';
 import { useMutation } from '@tanstack/react-query';
 import { login } from '@/service/auth/login';
-import { getUserInfo } from '@/service/user/getUserInfo';
 import { LoginProps, LoginResponseType } from '@/types/login';
 import { tokenStorage } from '@/service/auth/tokenStorage';
+import { useUserInfo } from './useUserInfo';
 
 export const useLogin = () => {
     const navigate = useNavigate();
-    const { setUser } = useUserStore();
     const { addToast } = useToastStore();
-
-    const handleGetUserInfo = async (email: string) => {
-        const userInfoResponse = await getUserInfo();
-        if (!userInfoResponse) {
-            addToast('유저 정보를 불러오지 못했습니다.', 'warning');
-            return false;
-        }
-        const userInfoWithEmail = {
-            nickname: userInfoResponse.nickname || '알 수 없음',
-            profileImageUrl: userInfoResponse.profileImageUrl || 'testimg.jpg',
-            email: email
-        };
-        setUser(userInfoWithEmail);
-        return true;
-    };
+    const { handleGetUserInfo } = useUserInfo();
 
     const mutation = useMutation({
         mutationFn: ({ email, password }: LoginProps) =>
             login({ email, password }),
-        onSuccess: (response: LoginResponseType, { email }) => {
+        onSuccess: (response: LoginResponseType) => {
             addToast('로그인 되었습니다.', 'success');
             tokenStorage.setAccessToken(response.result.accessToken);
-            handleGetUserInfo(email);
+            handleGetUserInfo();
             navigate('/');
         },
         onError: (error) => {
