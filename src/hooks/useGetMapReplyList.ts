6d55a66118edef70956e4714566de5509662984c
jaja <dkwjdals35@gmail.com>
2024-12-08 08:30:43 +0900

    feat : 지도 편지 상세 조회 답장 리스트 api 연동

---
+++ src/hooks/useGetMapReplyList.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { getMapReplyList } from '@/service/MapLetter/getMapReplyList';
+import { ReplyListResponseType } from '@/types/letter';
+
+type MapReplyListRequestProps = {
+    letterId: number;
+    page?: number;
+    size?: number;
+};
+
+export const useGetMapReplyList = ({
+    letterId,
+    page,
+    size
+}: MapReplyListRequestProps) => {
+    return useQuery<ReplyListResponseType, Error>({
+        queryKey: ['getMapReplyList', letterId, page, size],
+        queryFn: async () => {
+            const response = await getMapReplyList({
+                letterId,
+                page,
+                size
+            });
+            return response.result || { content: [] };
+        },
+        enabled: !!letterId
+    });
+};
