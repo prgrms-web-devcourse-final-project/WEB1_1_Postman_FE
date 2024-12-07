a56aafbf346616a1f6ab3252c06939a0a680c4e9
jaja <dkwjdals35@gmail.com>
2024-12-07 20:43:31 +0900

    feat : id props 삭제 및 api  연동

--- src/components/LetterDetailPage/LetterDatail/KeywordLetterDetail/KeywordLetterDetail.tsx
+++ src/components/LetterDetailPage/LetterDatail/KeywordLetterDetail/KeywordLetterDetail.tsx
@@ -19,13 +19,13 @@ type KeywordLetterDetailProps = {
 export const KeywordLetterDetail = ({
     letterData
 }: KeywordLetterDetailProps) => {
-    const { letterId, title, content, keywords, createdAt, font } = letterData;
+    const { title, content, keywords, createdAt, font } = letterData;
     return (
         <div className={clsx(font ? font : 'font-sans')}>
             <Margin top={20} />
             <div className="relative z-20 flex flex-col justify-center w-9/12 m-auto py-9">
                 <div className="absolute top-0 right-0">
-                    <DeleteButton id={String(letterId)} />
+                    <DeleteButton />
                 </div>
                 <h1>{title}</h1>
                 <img src={'/to_line.f4c129e6.svg'} className="w-full" />
