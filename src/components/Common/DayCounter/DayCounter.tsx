efedfea185a708be0603346fe9ccde1c8a91ad1a
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-07 16:24:54 +0900

    chore : daycounter 로직 수정

--- src/components/Common/DayCounter/DayCounter.tsx
+++ src/components/Common/DayCounter/DayCounter.tsx
@@ -1,21 +1,28 @@
 type DayCounterProps = {
     width?: string;
     height?: string;
-    daysLeft: number;
+    createdAt: string;
 };
 
 export const DayCounter = ({
     width = '66px',
     height = '30px',
-    daysLeft
+    createdAt
 }: DayCounterProps) => {
+    const currentDate = new Date();
+
+    const createdDate = new Date(createdAt);
+
+    const diffInMilliseconds = currentDate.getTime() - createdDate.getTime();
+    const daysPassed = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+
     return (
         <div
-            className="flex-center p-1 bg-gray-100 rounded-xl"
+            className="p-1 bg-gray-100 flex-center rounded-xl"
             style={{ width, height }}
         >
-            <span className="font-bold text-xs text-gray-500 text-nowrap">
-                D-{daysLeft}
+            <span className="text-xs font-bold text-gray-500 text-nowrap">
+                D+{daysPassed}일
             </span>
         </div>
     );
