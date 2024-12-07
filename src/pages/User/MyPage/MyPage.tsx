397f6c3bddf449a3e7cdc17aee2e56bb523b8b90
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-07 20:57:01 +0900

    chore : 마이페이지에 로그아웃 버튼 추가

--- src/pages/User/MyPage/MyPage.tsx
+++ src/pages/User/MyPage/MyPage.tsx
@@ -2,9 +2,14 @@ import { useUserStore } from '@/stores/useUserStore';
 import { ProfileSection } from '@/components/MyPage/ProfileSection';
 import { MenuListSection } from '@/components/MyPage/MenuListSection';
 import { KeywordListSection } from '@/components/MyPage/KeywordListSection';
+import { logout } from '@/service/auth/logout';
+import { useToastStore } from '@/hooks';
+import { useNavigate } from 'react-router-dom';
 
 export const MyPage = () => {
     const { user } = useUserStore();
+    const { addToast } = useToastStore();
+    const navigate = useNavigate();
 
     const sampleKeywords = [
         '키워드',
@@ -22,13 +27,24 @@ export const MyPage = () => {
 
     const labelMenuItems = [{ content: '라벨첩', url: '/labels' }];
 
+    const handleLogout = () => {
+        addToast('로그아웃 되었습니다.', 'success');
+        logout();
+        navigate('/login');
+    };
+
     return (
         <div className="flex flex-col gap-8">
             <h1 className="font-semibold text-gray-600 text-[21px]">
                 마이페이지
             </h1>
             <div className="flex flex-col gap-10">
-                <ProfileSection user={user!} />
+                <div className="flex flex-col gap-4 justify-end">
+                    <ProfileSection user={user!} />
+                    <div className="" onClick={handleLogout}>
+                        로그아웃
+                    </div>
+                </div>
                 <MenuListSection menuItems={menuItems} />
                 <MenuListSection menuItems={labelMenuItems} />
                 <KeywordListSection keywords={sampleKeywords} />
