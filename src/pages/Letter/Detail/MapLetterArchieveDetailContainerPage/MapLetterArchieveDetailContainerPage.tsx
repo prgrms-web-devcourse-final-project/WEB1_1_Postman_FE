82cc34f408acef771736443b2b276f095cb63c32
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-07 16:40:18 +0900

    chore: simple layout 수정

--- src/pages/Letter/Detail/MapLetterArchieveDetailContainerPage/MapLetterArchieveDetailContainerPage.tsx
+++ src/pages/Letter/Detail/MapLetterArchieveDetailContainerPage/MapLetterArchieveDetailContainerPage.tsx
@@ -1,10 +1,21 @@
+import { NavigationBar } from '@/components/Common/NavigationBar/NavigationBar';
+import { TopBar } from '@/components/Common/TopBar/TopBar';
 import { MapLetterArchieveDetailContainer } from '@/components/LetterDetailPage/LetterDetailContainer/MapLetterArchieveDetailContainer/MapLetterArchieveDetailContainer';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const MapLetterArchieveDetailContainerPage = () => {
+    const navigate = useNavigate();
+
+    const onBackClick = () => {
+        navigate(-1);
+    };
+
     return (
         <div>
+            <TopBar handleBackClick={onBackClick} />
             <MapLetterArchieveDetailContainer />
+            <NavigationBar />
         </div>
     );
 };
