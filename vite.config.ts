d7868131dc7c3e49957005d4632ee0e65f8d768c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-09 02:40:23 +0900

    chore : 빌드 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -17,14 +17,14 @@ export default defineConfig({
                 theme_color: '#ffffff',
                 icons: [
                     {
-                        src: 'icon-192x192.png',
+                        src: '/icon-192x192.svg',
                         sizes: '192x192',
-                        type: 'image/png'
+                        type: 'image/svg+xml'
                     },
                     {
-                        src: 'icon-512x512.png',
+                        src: '/icon-512x512.svg',
                         sizes: '512x512',
-                        type: 'image/png'
+                        type: 'image/svg+xml'
                     }
                 ]
             }
