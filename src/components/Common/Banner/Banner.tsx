d23bbf2838fbda7270abadf00f54d3acbf74d9f0
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-09 00:23:47 +0900

    style : 임시 배너 스타일 삽입

--- src/components/Common/Banner/Banner.tsx
+++ src/components/Common/Banner/Banner.tsx
@@ -7,7 +7,9 @@ type BannerProps = {
 
 export const Banner = ({ color, children }: BannerProps) => {
     return (
-        <div className={`flex h-20 rounded-lg shadow transition-all ${color}`}>
+        <div
+            className={`flex h-25 overflow-hidden rounded-lg shadow transition-all ${color}`}
+        >
             {children}
         </div>
     );
--- src/components/Common/Banner/LabelLotteryBanner.tsx
+++ src/components/Common/Banner/LabelLotteryBanner.tsx
@@ -2,11 +2,11 @@ import { NavLink } from 'react-router-dom';
 
 export const LabelLotteryBanner = () => {
     return (
-        <NavLink to={'/lottery'} className="flex items-center text-left w-full">
-            <div className="flex flex-col">
-                <p>라벨 뽑기</p>
-                <p>라벨 뽑으러가기</p>
-            </div>
+        <NavLink to={'/lottery'} className="w-full">
+            <img
+                src="/배너샘플1.png"
+                className="object-contain overflow-hidden"
+            />
         </NavLink>
     );
 };
