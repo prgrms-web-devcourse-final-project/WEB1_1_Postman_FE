d88cbabfe93e7f8033d96fdc9ec185d9755902b2
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-08 12:04:53 +0900

    style : 스타일 수정

--- src/components/SelectItemPage/AnimationBottle/AnimationBottle.tsx
+++ src/components/SelectItemPage/AnimationBottle/AnimationBottle.tsx
@@ -4,8 +4,8 @@ export const AnimationBottle = () => {
     return (
         <div className="flex justify-center w-full">
             <img
-                src="/유리병_샘플.png"
-                className="object-contain w-24 h-52 animate-wiggle animate-infinite animate-ease-linear"
+                src="/유리병.svg"
+                className="object-contain w-[35%] h-[35%] animate-wiggle animate-infinite animate-ease-linear"
             />
         </div>
     );
