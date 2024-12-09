79600156c0f027d82b7f3d176f40f20b2a23abe7
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-09 15:02:56 +0900

    chore : 알람 수정

--- src/hooks/usePushNotification .ts
+++ src/hooks/usePushNotification .ts
@@ -7,32 +7,23 @@ export const usePushNotification = () => {
     const { addToast } = useToastStore();
 
     useEffect(() => {
-        const isNotificationSet = localStorage.getItem('isNofication');
-
-        if (isNotificationSet === 'true') {
-            return;
-        }
-
-        // 알림 권한 요청
         Notification.requestPermission().then((permission) => {
             if (permission === 'granted') {
                 getToken(firebaseMessaging, {
                     vapidKey: import.meta.env.VITE_FCM_VAPID_KEY
                 })
                     .then((token) => {
-                        localStorage.setItem('isNofication', 'true');
+                        addToast('알람 설정을 성공했습니다.', 'success');
                         postToken({ token });
                     })
                     .catch((error) => {
-                        addToast('알람 설정에 실패했습니다.', 'error');
                         console.error(error);
                     });
             } else {
-                addToast('알람 설정을 거부했습니다.', 'success');
+                addToast('알람 설정을 거부했습니다.', 'warning');
             }
         });
 
-        // 알림 메시지를 받을 때마다 처리
         onMessage(firebaseMessaging, (payload) => {
             if (payload.notification) {
                 addToast(`${payload.notification.body}`, 'success');
