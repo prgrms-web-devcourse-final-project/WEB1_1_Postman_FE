5a96b69b0d1dfc2275acf3700f65607f734577d0
kangminguu <mingudat9909@gmail.com>
2024-12-09 09:41:06 +0900

    chore : 메인페이지 추천/답장 편지 라우팅 경로 수정

--- src/components/HomePage/HomeBottleLetter.tsx
+++ src/components/HomePage/HomeBottleLetter.tsx
@@ -4,7 +4,7 @@ import { Label } from '../Common/BottleLetter/Label/Label';
 import { useNavigate } from 'react-router-dom';
 
 interface HomeBottleLetterProps {
-    letterType: 'keyword' | 'reply';
+    letterType: 'LETTER' | 'REPLY_LETTER';
     labelUrl: string;
     letterId: number;
 }
@@ -18,8 +18,19 @@ export const HomeBottleLetter = ({
     const navigate = useNavigate();
 
     const handleClickBottle = () => {
-        navigate(`/letter/${letterType}/${letterId}`);
+        navigate(`letter/keyword/${letterType}/received/${letterId}`);
     };
+
+    console.log('letterId - ', letterType);
+    console.log('letterId - ', letterId);
+
+    // 키워드 편지 경로
+    // /letter/keyword/LETTER/received/49
+
+    // + 답장의 경우 경로?
+    // + 아직 지도 편지 답장 경로는 설정하지 않았습니다.
+    // /letter/map/received/3
+    // /letter/keyword/REPLY_LETTER/received/5
 
     return (
         <div className="relative w-full h-full">
