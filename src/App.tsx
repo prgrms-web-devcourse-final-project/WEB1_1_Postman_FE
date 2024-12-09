5b82d391ad0710b9917139740ede67addf18d16b
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-09 17:11:34 +0900

    feat : nofication hook 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from '@/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { usePushNotification } from './hooks/usePushNotification ';
 
 export const App = () => {
     const queryClient = new QueryClient();
-    usePushNotification();
     return (
         <QueryClientProvider client={queryClient}>
             <ToastContainer />
