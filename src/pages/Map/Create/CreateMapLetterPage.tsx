5ec4168110df3ec2c4d1ff3cd7b75cfaff5a96dd
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-09 15:04:26 +0900

    feat : 지도 편지 생성

---
+++ src/pages/Map/Create/CreateMapLetterPage.tsx
@@ -0,0 +1,10 @@
+import CreateMapLetterCotainer from '@/components/CreateMapLetter/CreateMapLetterCotainer/CreateMapLetterCotainer';
+import React from 'react';
+
+export const CreateMapLetterPage = () => {
+    return (
+        <div className="w-full h-full">
+            <CreateMapLetterCotainer />
+        </div>
+    );
+};
