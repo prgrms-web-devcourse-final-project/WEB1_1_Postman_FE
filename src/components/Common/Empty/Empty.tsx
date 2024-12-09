0ec9a0fa04497e6b3e53c41fc43993e47c4750ba
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2024-12-09 22:26:48 +0900

    feat : 로딩스피너 컴포넌트 / 빈 리스트 컴포넌트 추가

---
+++ src/components/Common/Empty/Empty.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { Margin } from '../Margin/Margin';
+
+export const Empty = () => {
+    const navigate = useNavigate();
+    return (
+        <div className=" flex flex-col items-center justify-center">
+            <Margin top={250} />
+            <h1 className="text-5xl font-bold mb-4 text-sample-blue">텅</h1>
+            <div className="text-xl font-semibold text-gray-700 text-center">
+                아직 편지가 발견되지 않았어요.
+            </div>
+            <p className="mt-2 text-center mb-8">
+                지구 이곳저곳에서 편지를 찾아보거나
+                <br />
+                마음에 드는 곳에 편지를 흘려 보세요.
+            </p>
+            <button
+                className="mt-2 px-4 py-2 bg-sample-blue text-white rounded-md"
+                onClick={() => navigate('/mapexplorer')}
+            >
+                여행하러 가기
+            </button>
+        </div>
+    );
+};
