cb8cb0e3b33cdbd3a3f7d45ead256b1d7c38d3c2
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-23 16:48:58 +0900

    chore : ligthhouse 성능 수정

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -11,11 +11,17 @@ module.exports = {
                 'categories:performance': ['warn', { minScore: 0.7 }],
                 'categories:accessibility': ['warn', { minScore: 0.7 }],
                 'categories:seo': ['warn', { minScore: 0.7 }],
-                'categories:best-practices': ['warn', { minScore: 0.7 }]
+                'categories:best-practices': ['warn', { minScore: 0.7 }],
+                'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
+                'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
+                'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }]
             }
         },
         upload: {
-            target: 'temporary-public-storage'
+            target: 'filesystem',
+            outputDir: './lhci_reports',
+            reportFilenamePattern:
+                '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%'
         },
         server: {
             port: 9000
