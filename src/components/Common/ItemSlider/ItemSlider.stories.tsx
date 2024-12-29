5d3a3bef6b2e9994095250c6561c8a8d6934cefb
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-12-30 02:00:40 +0900

    story : story 수정

--- src/components/Common/ItemSlider/ItemSlider.stories.tsx
+++ src/components/Common/ItemSlider/ItemSlider.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ItemSlider } from './ItemSlider';
-import { useMemo } from 'react';
 import SkyTheme from '@/asset/letter1/letter1.svg?react';
 import HaertTheme from '@/asset/letter2/letter2.svg?react';
 import FlowerTheme from '@/asset/letter3/letter3.svg?react';
@@ -15,26 +14,21 @@ export default meta;
 
 type Story = StoryObj<typeof ItemSlider>;
 
-const textItems = useMemo(
-    () => [
-        { name: 'font-sans', id: '1', fontName: '기본 폰트' },
-        { name: 'font-bagelfatone', id: '2', fontName: '베이글 폰트' },
-        { name: 'font-cookierun', id: '3', fontName: '쿠키런 폰트' },
-        { name: 'font-pyeongchangpeace', id: '4', fontName: '평창 폰트' },
-        { name: 'font-sagak', id: '5', fontName: '필기체 폰트' },
-        { name: 'font-serif', id: '6', fontName: '세리프 폰트' }
-    ],
-    []
-);
+const textItems = [
+    { name: 'font-sans', id: '1', fontName: '기본 폰트' },
+    { name: 'font-bagelfatone', id: '2', fontName: '베이글 폰트' },
+    { name: 'font-cookierun', id: '3', fontName: '쿠키런 폰트' },
+    { name: 'font-pyeongchangpeace', id: '4', fontName: '평창 폰트' },
+    { name: 'font-sagak', id: '5', fontName: '필기체 폰트' },
+    { name: 'font-serif', id: '6', fontName: '세리프 폰트' }
+];
 
-const imageItems = useMemo(
-    () => [
-        { id: '1', src: SkyTheme, name: '이미지' },
-        { id: '2', src: HaertTheme, name: '이미지' },
-        { id: '3', src: FlowerTheme, name: '이미지' }
-    ],
-    []
-);
+const imageItems = [
+    { id: '1', src: SkyTheme, name: '이미지' },
+    { id: '2', src: HaertTheme, name: '이미지' },
+    { id: '3', src: FlowerTheme, name: '이미지' }
+];
+
 export const TextSlider: Story = {
     args: {
         itemType: 'text',
