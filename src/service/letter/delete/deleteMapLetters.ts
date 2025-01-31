50d412e44c89a8e7cf115b8be641d77e2ca9dd6c
cmlim0070 <87525734+cmlim0070@users.noreply.github.com>
2025-02-01 02:53:37 +0900

    chore : 지도편지 삭제 타입 변경

--- src/service/letter/delete/deleteMapLetters.ts
+++ src/service/letter/delete/deleteMapLetters.ts
@@ -2,13 +2,13 @@ import { defaultApi } from '@/service/api';
 import { ApiResponseType } from '@/types/apiResponse';
 
 type DeleteLetterType = {
-    letterId: number;
+    letterIds: number[];
 };
 
 type deleteMapLettersResponse = ApiResponseType<string>;
 
 export const deleteMapLetters = async (
-    selectedList: DeleteLetterType[]
+    selectedList: DeleteLetterType
 ): Promise<deleteMapLettersResponse> => {
     const api = defaultApi();
     const response = await api.delete('/map', {
