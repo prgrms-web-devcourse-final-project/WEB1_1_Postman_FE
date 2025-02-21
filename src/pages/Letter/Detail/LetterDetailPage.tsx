363e454d28332d43f33d90bab288811b7bebafd0
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-02-21 11:32:12 +0900

    feat : 편지 상세보기 topbar 추가
    편지 상세보기 페이지에 topbar를 추가했습니다.

--- src/pages/Letter/Detail/LetterDetailPage.tsx
+++ src/pages/Letter/Detail/LetterDetailPage.tsx
@@ -1,9 +1,11 @@
 import { NavigationBar } from '@/components/Common/NavigationBar/NavigationBar';
+import { TopBar } from '@/components/Common/TopBar/TopBar';
 import { LetterDetailContainer } from '@/components/LetterDetailPage/LetterDetailContainer/LetterDetailContainer';
 
 const LetterDetailPage = () => {
     return (
         <div className="w-full min-h-screen">
+            <TopBar />
             <LetterDetailContainer />
             <NavigationBar />
         </div>
